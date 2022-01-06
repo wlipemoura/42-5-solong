a6ba415eb244efe743cd995d6f58660814222971
William Felipe dos Santos Moura <felipe.1.william@gmail.com>
2022-01-06 20:03:10 +0100

    elements verifier now check if there is the minimum of elements

--- map_verifier.c
+++ map_verifier.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_verifier.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 19:22:09 by coder             #+#    #+#             */
+/*   Updated: 2022/01/06 20:01:35 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <unistd.h>
 #include <stdio.h>
@@ -10,9 +22,9 @@
 #define WALL '1'
 #define START 'P'
 #define PATH '0'
-#define TRUE 0 //?
-#define FALSE 1 //?
-
+#define TRUE 1 //?
+#define FALSE 0 //?
+#define INVALID_MAP "Please, input a valid map.\n"
 /* 1. read a file 
 - Is it empty? return error
 - create a matrix to it;
@@ -24,7 +36,6 @@
 
 1. What is the necessary number of rows and columns of the matrix? */
 
-
 typedef struct s_map
 {
 	int	width;
@@ -33,35 +44,36 @@ typedef struct s_map
 
 char	*new_get_next_line(int fd);
 
-int	elements_verifier(char *map_arrayed)//return height
+int	elements_verifier(char *map_arrayed)//guarantee that it is a valid array (in new_gnl function)
 {
 	int	n_rows;
 	int	index;
 
 	n_rows = 1;
 	index = 0;
-	if (!map_arrayed)
-		return (FALSE);
 	while (*(map_arrayed + index))
 	{
 		if (*(map_arrayed + index) == '\n')
 			n_rows++;
 		index++;
 	}
+	if (!ft_strchr(map_arrayed, EXIT) || !ft_strchr(map_arrayed, COLLECTIBLE)
+			|| !ft_strchr(map_arrayed, WALL) || !ft_strchr(map_arrayed, START)
+			|| !ft_strchr(map_arrayed, PATH))
+			return (NULL);
 	while (*map_arrayed)
 	{
-		if (*(map_arrayed) != COLLECTIBLE
-		&& *(map_arrayed) != EXIT && *(map_arrayed) != WALL
-		&& *(map_arrayed) != START && *(map_arrayed) != PATH
-		&& *(map_arrayed) != '\n')
-			return (FALSE);
+		if ((*(map_arrayed) != COLLECTIBLE && *(map_arrayed) != EXIT
+			&& *(map_arrayed) != WALL && *(map_arrayed) != START
+			&& *(map_arrayed) != PATH && *(map_arrayed) != '\n'))
+				return (NULL);
 		else
 			map_arrayed++;
 	}
 	return (n_rows);
 }
 
-int	matrix_creator(char *map_arrayed, char **matrix, t_map map)//return height
+int	matrix_creator(char *map_arrayed, char **matrix, t_map map)
 {
 	int		index;
 	int		map_fd;
@@ -76,30 +88,14 @@ int	matrix_creator(char *map_arrayed, char **matrix, t_map map)//return height
 	return (0);
 }
 
-int	matrix_height(char *map_arrayed)
-{
-	int	n_rows;
-	int	index;
-
-	n_rows = 1;
-	index = 0;
-	while (*(map_arrayed + index))
-	{
-		if (*(map_arrayed + index) == '\n')
-			n_rows++;
-		index++;
-	}
-	return (n_rows);
-}
-
-int	map_format_verifier(char *map_arrayed)//return width
+int	map_format_verifier(char *map_arrayed)
 {
 	int	width_reference;
 	int	i_element;
 	int	i_row_begin;
 
 	i_element = 0;
-	while(*(map_arrayed + i_element) && *(map_arrayed + i_element) != '\n')
+	while (*(map_arrayed + i_element) && *(map_arrayed + i_element) != '\n')
 		i_element++;
 	width_reference = i_element;
 	while (*(map_arrayed + i_element))
@@ -120,15 +116,16 @@ int	map_border_verifier(char **map_matrix, t_map map)
 {
 	int		i_row;
 	int		i_column;
-	
+
 	i_row = 0;
 	i_column = 0;
-	
 	while (i_row < map.height)
 	{
-		if((i_row == 0 || i_row == map.height - 1 || i_column == 0
-		|| i_column == map.width - 1) && map_matrix[i_row][i_column] != WALL)
-			return (FALSE);
+		//printf("i_row = %d, i_column = %d\n", i_row, i_column);
+		if ((i_row == 0 || i_row == map.height - 1 || i_column == 0
+				|| i_column == map.width - 1)
+			&& map_matrix[i_row][i_column] != WALL)
+				return (FALSE);
 		i_column++;
 		if (i_column == map.width)
 		{
@@ -139,7 +136,7 @@ int	map_border_verifier(char **map_matrix, t_map map)
 	return (TRUE);
 }
 
-int	main(void)
+int	main(void) //change name to map_verifier
 {
 	char		*map_arrayed;
 	static char	**matrix;
@@ -148,12 +145,21 @@ int	main(void)
 
 	map_fd = open("mini_map.ber", O_RDONLY);
 	map_arrayed = new_get_next_line(map_fd);
-	map.height = elements_verifier(map_arrayed);
-	map.width = map_format_verifier(map_arrayed);
+	if ( !(map.height = elements_verifier(map_arrayed))
+		|| !(map.width = map_format_verifier(map_arrayed)))
+	{
+		printf("%s", INVALID_MAP);
+		return (NULL);
+	}
 	printf("Matrix height will be: %d\n", map.height);
 	printf("Matrix width will be: %d\n", map.width);
 	matrix = (char **) ft_calloc(map.height, sizeof(char *));
 	matrix_creator(map_arrayed, matrix, (t_map){map.height, map.width});
+	if (map_border_verifier(matrix, map) == NULL)
+	{
+		printf("%s", INVALID_MAP);
+		return (NULL);
+	}
 	///printf("Matrix[0][0] = %c\n", matrix[0][0]);
 	//printf("Matrix[0][1] = %c\n", matrix[2][1]);
 	//printf("Is it closed by walls? %d\n", (map_border_verifier(matrix, map)));
