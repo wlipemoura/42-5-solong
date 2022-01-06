effe18b3a8bdb77b4606c2bfde314dac7ec5ef43
William Felipe dos Santos Moura <felipe.1.william@gmail.com>
2022-01-06 19:17:33 +0100

    elements verifier incorporation on main

--- map_verifier.c
+++ map_verifier.c
@@ -1,21 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   map_verifier.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/05 18:14:24 by coder             #+#    #+#             */
-/*   Updated: 2022/01/06 16:05:47 by coder            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include "./42-1-libft/libft.h"
+
+#define COLLECTIBLE 'C'
+#define EXIT 'E'
+#define WALL '1'
+#define START 'P'
+#define PATH '0'
+#define TRUE 0 //?
+#define FALSE 1 //?
 
 /* 1. read a file 
 - Is it empty? return error
@@ -28,21 +24,15 @@
 
 1. What is the necessary number of rows and columns of the matrix? */
 
+
 typedef struct s_map
 {
 	int	width;
 	int	height;
 }	t_map;
 
-#define COLLECTIBLE 'C'
-#define EXIT 'E'
-#define WALL '1'
-#define START 'P'
-#define PATH '0'
-#define TRUE 0 //?
-#define FALSE 1 //?
-#define ERROR 1
-//ONLY THE 5 EXPECTED ELEMENTS COMPOUND THE MAP?
+char	*new_get_next_line(int fd);
+
 int	elements_verifier(char *map_arrayed)//return height
 {
 	int	n_rows;
@@ -70,34 +60,23 @@ int	elements_verifier(char *map_arrayed)//return height
 	}
 	return (n_rows);
 }
-//IS THE MAP RETANGULAR?
-int	map_format_verifier(char *map_arrayed)//return width
-{
-	int	width_reference;
-	int	i_element;
-	int	i_row_begin;
 
-	i_element = 0;
-	while(*(map_arrayed + i_element) && *(map_arrayed + i_element) != '\n')
-		i_element++;
-	width_reference = i_element;
-	while (*(map_arrayed + i_element))
+int	matrix_creator(char *map_arrayed, char **matrix, t_map map)//return height
+{
+	int		index;
+	int		map_fd;
+
+	index = 0; //guarantee that the array has at least one character before enter in this function
+	map_fd = open("mini_map.ber", O_RDONLY);//this one is just for a test. I need to put argc, argv
+	while (index < map.height)
 	{
-		i_row_begin = ++i_element;
-		printf("i_row_begin = %d\n", i_row_begin);
-		while (*(map_arrayed + i_element) != '\n' && *(map_arrayed + i_element))
-			i_element++;
-		printf("i_row_end = %d\n", i_element);
-		if ((i_element - i_row_begin) != width_reference)
-			return (FALSE);
+		matrix[index] = get_next_line(map_fd, map.width + 1);//adapt get_next_line to get buffer_size
+		index++;
 	}
-	if ((i_element - i_row_begin) != width_reference)
-		return (FALSE);
-	else
-		return (width_reference);
+	return (0);
 }
 
-int	matrix_height(char *map_arrayed)//I will be able to exclude this function. I will replace it by the elements verifier
+int	matrix_height(char *map_arrayed)
 {
 	int	n_rows;
 	int	index;
@@ -113,22 +92,30 @@ int	matrix_height(char *map_arrayed)//I will be able to exclude this function. I
 	return (n_rows);
 }
 
-//CREATE A MATRIX TO VERIFY BORDERS
-int	matrix_creator(char *map_arrayed, char **matrix, t_map map)//return height
+int	map_format_verifier(char *map_arrayed)//return width
 {
-	int		index;
-	int		map_fd;
+	int	width_reference;
+	int	i_element;
+	int	i_row_begin;
 
-	index = 0; //guarantee that the array has at least one character before enter in this function
-	map_fd = open("mini_map.ber", O_RDONLY);//this one is just for a test. I need to put argc, argv
-	while (index < map.height)
+	i_element = 0;
+	while(*(map_arrayed + i_element) && *(map_arrayed + i_element) != '\n')
+		i_element++;
+	width_reference = i_element;
+	while (*(map_arrayed + i_element))
 	{
-		matrix[index] = get_next_line(map_fd, map.width + 1);//adapt get_next_line to get buffer_size
-		index++;
+		i_row_begin = ++i_element;
+		while (*(map_arrayed + i_element) != '\n' && *(map_arrayed + i_element))
+			i_element++;
+		if ((i_element - i_row_begin) != width_reference)
+			return (FALSE);
 	}
-	return (0);
+	if ((i_element - i_row_begin) != width_reference)
+		return (FALSE);
+	else
+		return (width_reference);
 }
-//IS THE MAP ROUNDED BY WALLS?
+
 int	map_border_verifier(char **map_matrix, t_map map)
 {
 	int		i_row;
@@ -136,47 +123,38 @@ int	map_border_verifier(char **map_matrix, t_map map)
 	
 	i_row = 0;
 	i_column = 0;
-	while (i_column < map.width)
-	{
-		if (map_matrix[i_row][i_column] != WALL)
-			return (FALSE);
-		else
-			i_column++;
-		if (i_row == 0)
-		{
-			i_row == map.height - 1;
-			i_column == 0;
-		}
-	}
+	
 	while (i_row < map.height)
 	{
-		if (map_matrix[i_row][i_column] != WALL)
+		if((i_row == 0 || i_row == map.height - 1 || i_column == 0
+		|| i_column == map.width - 1) && map_matrix[i_row][i_column] != WALL)
 			return (FALSE);
-		else
-			i_row++;
-		if (i_column == 0)
+		i_column++;
+		if (i_column == map.width)
 		{
-			i_column == map.width - 1;
-			i_row == 0;
+			i_column = 0;
+			i_row++;
 		}
 	}
-
+	return (TRUE);
 }
 
-int	map_verifier(int fd)
+int	main(void)
 {
-	static char	**matrix;
 	char		*map_arrayed;
+	static char	**matrix;
 	t_map		map;
-	int			i_row;
+	int			map_fd;
 
-	map_arrayed = new_get_next_line(fd);
-	if (!elements_verifier(map_arrayed))
-		return (ERROR);
-	else if (!(map.width=map_format_verifier(map_arrayed)))
-		return (ERROR);
-	map.height = map_height(map_arrayed);
-	matrix = (char *) ft_calloc(map.height, sizeof(char *));
+	map_fd = open("mini_map.ber", O_RDONLY);
+	map_arrayed = new_get_next_line(map_fd);
+	map.height = elements_verifier(map_arrayed);
+	map.width = map_format_verifier(map_arrayed);
+	printf("Matrix height will be: %d\n", map.height);
+	printf("Matrix width will be: %d\n", map.width);
+	matrix = (char **) ft_calloc(map.height, sizeof(char *));
 	matrix_creator(map_arrayed, matrix, (t_map){map.height, map.width});
+	///printf("Matrix[0][0] = %c\n", matrix[0][0]);
+	//printf("Matrix[0][1] = %c\n", matrix[2][1]);
+	//printf("Is it closed by walls? %d\n", (map_border_verifier(matrix, map)));
 }
-
