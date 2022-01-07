3b40d58b673ef5394e6b9861aae3d9f36d70441d
William Felipe dos Santos Moura <felipe.1.william@gmail.com>
2022-01-07 19:24:54 +0100

    libft setted

--- map_validator.c
+++ map_validator.c
@@ -1,27 +1,12 @@
 #include "so_long.h"
 
-int	ft_strrncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	len_s1;
-	size_t	len_s2;
-
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	while (*(s1 + len_s1 - n) && *(s2 + len_s2 - n) && n != 0
-		&& (*(s1 + len_s1 - n) == *(s2 + len_s2 - n)))
-		n--;
-	if ((*(s1 + len_s1 - n) != *(s2 + len_s2 - n) && n))
-		return (*(unsigned char *)(s1 + len_s1 - n)
-		- *(unsigned char *)(s2 + len_s2 - n));
-	return (0);
-}
 
 //int	main(int argc, char **argv)
 int	main(void)
 {
 	char *map_dir;
 	//Quando for testar com argc, comentar este bloco
-	map_dir = "map_test.ber";
+	map_dir = "subject.ber";
 	if (ft_strrncmp(map_dir, ".ber", 4) != 0)
 	{
 		printf("%s", INVALID_MAP);
@@ -38,5 +23,6 @@ int	main(void)
 	else
 		map_verifier(argv[1]); */
 	//gcc -g map_validator.c map_verifier.c ./42-1-libft/*.c -fsanitize=address && ./a.out mini_map.ber
+	//or gcc -g map_validator.c map_verifier.c -L./42-1-libft/ -lft
 	
 }
